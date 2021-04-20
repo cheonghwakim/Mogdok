7666d2a0d9c008b625618fb4868542cd5ba11976
dudcheol <95_pyc@naver.com>
2021-04-20 11:51:49 +0900

    feat: 🎸 setting - [FE] 프로젝트 초기 설정
    ✅ Closes: S04P31A401-55

---
+++ FrontEnd/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  //lifecycle area
+  methods: {},
+};
+</script>
+
+<style></style>
