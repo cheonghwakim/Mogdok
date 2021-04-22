264a0c57d8791321be6dab8475512c6435f9bd32
dudcheol <95_pyc@naver.com>
2021-04-22 13:17:55 +0900

    feat: 🎸 toy/webrtc - [FE] toy 프로젝트를 위한 환경 세팅
    ✅ Closes: S04P31A401-55

---
+++ toy/webrtc/src/components/OpenVidu.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    <h1>몽실이네 독서실 WebRTC compo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OpenVidu',
+  props: {},
+};
+</script>
+
+<style scoped></style>
