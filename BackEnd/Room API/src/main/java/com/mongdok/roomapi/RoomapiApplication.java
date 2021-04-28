856f7240c4f7248cb599aa96ed345d22a04c12e2
hansol kim <doingnow94@gmail.com>
2021-04-28 18:15:07 +0900

    feat: 🎸 Room API 서버 구축 및 disconnect 요청 구현
    Room API 서버 구축 및 disconnect controller 구현
    
    ✅ Closes: S04P31A401-131

---
+++ BackEnd/Room API/src/main/java/com/mongdok/roomapi/RoomapiApplication.java
@@ -0,0 +1,13 @@
+package com.mongdok.roomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomapiApplication.class, args);
+    }
+
+}
