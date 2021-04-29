bcf5c28eee03311dc56a9490405a1d5d2e54d7af
이주상 <wntkd0530@naver.com>
2021-04-29 23:25:09 +0900

    feat: 🎸 compo - [FE] 하단 버튼 그룹 구현 완료 및 반응형 수정
    ✅ Closes: #S04P31A401-84

--- FrontEnd/src/components/common/RoomFooter.vue
+++ FrontEnd/src/components/common/RoomFooter.vue
@@ -1,42 +1,119 @@
 <template lang="">
-   <div class="footer floating">
+   <div @mouseover="showFooter" class="footer floating" :class="[isOpen ? 'jumping' : '', 'borrow']">
+      <div class="postit-wrapper">
+         <div-post-it :type="'study'" :timer="'10:30:20'"></div-post-it>
+      </div>
+      <div class="content">
+         <btn-my-desk class="btnMyDesk-wrapper"></btn-my-desk>
+         <btn-command class="btnCommand-wrapper" :label="'공부 하자!'"></btn-command>
+         <btn-leave-desk class="btnLeaveDesk-wrapper"></btn-leave-desk>
+      </div>
       <div class="img-wrapper">
          <img src="@/assets/img/noteLong.svg" alt="" />
       </div>
    </div>
 </template>
 <script>
+import DivPostIt from '@/components/ui/DivPostIt';
+import BtnCommand from '@/components/ui/BtnCommand';
+import BtnMyDesk from '@/components/ui/BtnMyDesk';
+import BtnLeaveDesk from '@/components/ui/BtnLeaveDesk';
+
 export default {
    name: 'Footer',
-   components: {},
+   components: { DivPostIt, BtnCommand, BtnMyDesk, BtnLeaveDesk },
    props: {},
    data() {
-      return {};
+      return {
+         isOpen: true,
+      };
    },
    computed: {},
-   watch: {},
+   // watch: {},
    //lifecycle area
-   methods: {},
+   methods: {
+      showFooter: function() {
+         var footer = document.querySelector('.footer.floating');
+         console.log(footer);
+         // footer.classList.add('borrow');
+      },
+   },
 };
 </script>
 <style scoped lang="scss">
 @import 'src/assets/css/common';
 
+.borrow {
+   animation: burrowing 2s 1s ease forwards;
+}
+
+.jumping {
+   animation: burrowing-reverse 2s 1s ease forwards;
+}
+
 .footer {
    bottom: 0;
    left: 50%;
    transform: translate(-50%, 20%);
+
+   /* animation: burrowing-reverse 2s 1s ease forwards; */
    /* background-color: rgb(173, 199, 254); */
    /* height: $footerHeight; */
 
    width: 50vw;
    max-width: 600px;
-   min-width: 375px;
 
    height: auto;
 
+   .postit-wrapper {
+      position: absolute;
+      top: -10%;
+      left: 50%;
+      transform: translateX(-50%);
+
+      width: 15vw;
+      min-width: 100px;
+      max-width: 160px;
+   }
+
+   .content {
+      /* background-color: blue; */
+      width: 100%;
+      position: absolute;
+      top: 30%;
+
+      .btnMyDesk-wrapper {
+         position: absolute;
+         left: 10%;
+         width: 7vw;
+
+         min-width: 50px;
+         max-width: 70px;
+      }
+
+      .btnCommand-wrapper {
+         position: absolute;
+         left: 50%;
+         transform: translateX(-50%);
+
+         width: 15vw;
+         min-width: 100px;
+         max-width: 150px;
+      }
+
+      .btnLeaveDesk-wrapper {
+         position: absolute;
+         right: 10%;
+         width: 10vw;
+
+         min-width: 50px;
+         max-width: 100px;
+      }
+   }
+
    //flex-shrink: 0; // flex에 영향 없는 고정 요소 셋팅
    .img-wrapper {
+      overflow: hidden;
       img {
          width: 100%;
          height: auto;
@@ -47,23 +124,37 @@ export default {
    }
 }
 
+@include mobile {
+   .footer {
+      /* border: 2px solid red; */
+      width: 70vw;
+   }
+}
+
 @include tinyMobile {
    .footer {
-      border: 1px solid red;
-      min-width: 100vw;
-      min-height: $footerHeight;
-      /* transform: scale(1.1); */
+      /* border: 2px solid blue; */
+      width: 100vw;
+   }
+}
 
-      .img-wrapper {
-         height: 190px;
-         img {
-            height: 100%;
-            width: auto;
+// 가운데 정렬 되어 있는 요소를 버로우 시키기
+@keyframes burrowing {
+   0% {
+      transform: translate(-50%, 20%);
+   }
+   100% {
+      transform: translate(-50%, 75%);
+   }
+}
 
-            object-fit: cover;
-            object-position: 50% 0px;
-         }
-      }
+// 가운데 정렬 되어 있는 요소를 버로우 시키기
+@keyframes burrowing-reverse {
+   0% {
+      transform: translate(-50%, 75%);
+   }
+   100% {
+      transform: translate(-50%, 20%);
    }
 }
 </style>
