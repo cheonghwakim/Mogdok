e58beffd643962dff565999e342d25aa5e2fa993
dudcheol <95_pyc@naver.com>
2021-04-29 18:31:46 +0900

    feat: 🎸 toy/calendar - [FE] 스터디 캘린더 전체 기능 구현
    ✅ Closes: S04P31A401-102

--- toy/calendar/src/components/StudyCalendar.vue
+++ toy/calendar/src/components/StudyCalendar.vue
@@ -19,14 +19,33 @@
             <td
               v-for="(day, secondIdx) in date"
               :key="secondIdx"
-              :class="{
-                'calendar-today': day === today && month === currentMonth && year === currentYear,
-                'calendar-pre-month': idx === 0 && day >= lastMonthStart,
-                'calendar-next-month': dates.length - 1 === idx && nextMonthStart > day,
-              }"
-              class="day-box"
+              :class="[
+                {
+                  'calendar-today': day === today && month === currentMonth && year === currentYear,
+                  'calendar-pre-month': idx === 0 && day >= lastMonthStart,
+                  'calendar-next-month': dates.length - 1 === idx && nextMonthStart > day,
+                },
+                `orange-${convertRunningTimeToColorLevel(
+                  convertMsToHour(studyTimeCalendar[day].runningTime)
+                )}`,
+              ]"
+              class="day-box tooltip"
+              @mouseover="getDetailStudyTimeOfSelectedDate(day)"
             >
               {{ day }}
+              <div class="tooltip-box">
+                {{ selectedDate }}
+                <div class="day-container" :key="selectedDate">
+                  <div
+                    v-for="(hour, index) in selectedDateStudyHourList"
+                    :key="'detail' + index"
+                    class="day-detail-box"
+                    :class="{ 'has-study-hour': hour }"
+                  >
+                    {{ index }}
+                  </div>
+                </div>
+              </div>
             </td>
           </tr>
         </tbody>
@@ -48,7 +67,49 @@ export default {
       lastMonthStart: 0,
       nextMonthStart: 0,
       today: 0,
+      showDetailStudyTime: false,
+      selectedDate: '',
+      selectedDateStudyHourList: new Array(24),
+      studyTimeCalendar: undefined,
+      // 테스트데이터
+      studyTimestamp: [
+        {
+          studyId: 1,
+          studyTime: '2021-04-28T00:00:00.000',
+          startTime: '2021-04-28T00:00:00.000',
+          status: 'PAUSE',
+        },
+        {
+          studyId: 2,
+          studyTime: '2021-04-28T01:00:00.000',
+          startTime: '2021-04-28T00:00:00.000',
+          status: 'START',
+        },
+        {
+          studyId: 3,
+          studyTime: '2021-04-28T04:00:00.000',
+          startTime: '2021-04-28T00:00:00.000',
+          status: 'END',
+        },
+      ],
     };
+  },
+  watch: {
+    month: {
+      immediate: true,
+      handler(value) {
+        // 변경된 달로 axios 요청을 보내고 수신한 결과를 달력에 뿌려준다
+        // 서버로부터 받아온 그 달의 공부시간 기록 데이터를 이용해 달력과 맞춘다
+        this.studyTimeCalendar = this.convertStudyTimeMonthToCalendar(
+          this.getStudyTimeMonth(this.year, value)
+        );
+        console.log(
+          '%cStudyCalendar.vue line:104 this.studyTimeCalendar',
+          'color: #007acc;',
+          this.studyTimeCalendar
+        );
+      },
+    },
   },
   created() {
     // 데이터에 접근이 가능한 첫 번째 라이프 사이클
@@ -59,9 +120,6 @@ export default {
     this.month = this.currentMonth;
     this.today = date.getDate(); // 오늘 날짜
     this.calendarData();
-  },
-  mounted() {
-    this.parseStudyTimeOfDay();
   },
   methods: {
     calendarData(arg) {
@@ -132,68 +190,10 @@ export default {
       this.nextMonthStart = weekOfDays[0]; // 이번 달 마지막 주에서 제일 작은 날짜
       return dates;
     },
-    parseStudyTimeOfDay() {
-      // 테스트데이터
-      const studyTimestamp = [
-        {
-          studyId: 1,
-          studyTime: '2021-04-28T00:00:00.000',
-          startTime: '2021-04-28T00:00:00.000',
-          status: 'PAUSE',
-        },
-        {
-          studyId: 2,
-          studyTime: '2021-04-28T01:00:00.000',
-          startTime: '2021-04-28T00:00:00.000',
-          status: 'START',
-        },
-        {
-          studyId: 3,
-          studyTime: '2021-04-28T03:00:00.000',
-          startTime: '2021-04-28T00:00:00.000',
-          status: 'PAUSE',
-        },
-        {
-          studyId: 2,
-          studyTime: '2021-04-28T04:00:00.000',
-          startTime: '2021-04-28T00:00:00.000',
-          status: 'START',
-        },
-        {
-          studyId: 3,
-          studyTime: '2021-04-28T05:00:00.000',
-          startTime: '2021-04-28T00:00:00.000',
-          status: 'PAUSE',
-        },
-        {
-          studyId: 4,
-          studyTime: '2021-04-29T04:00:00.000',
-          startTime: '2021-04-28T00:00:00.000',
-          status: 'END',
-        },
-        {
-          studyId: 1,
-          studyTime: '2021-04-29T00:00:00.000',
-          startTime: '2021-04-29T00:00:00.000',
-          status: 'PAUSE',
-        },
-        {
-          studyId: 2,
-          studyTime: '2021-04-28T04:00:00.000',
-          startTime: '2021-04-28T00:00:00.000',
-          status: 'START',
-        },
-        {
-          studyId: 4,
-          studyTime: '2021-04-29T04:00:00.000',
-          startTime: '2021-04-28T00:00:00.000',
-          status: 'END',
-        },
-      ];
+    //==================================================================================================================================
+    parseStudyTimeOfDay(studyTimestamp) {
       const size = studyTimestamp.length;
       const studyRunningTimeList = [];
-
-      // let hourToMs = 1000 * 60 * 60;
       let flag = false;
       let curStartTime = null;
       let runningTime = 0;
@@ -213,16 +213,84 @@ export default {
               startTime: new Date(startTime + 'Z'), // 로컬 시간으로 변환해서 기록
               runningTime,
             });
-            console.log(
-              '%cStudyCalendar.vue line:210 runningTime',
-              'color: #007acc;',
-              runningTime / (1000 * 60 * 60)
-            );
             runningTime = 0; // 공부가 끝났으므로 다음 공부시간 기록을 위해 초기화
           }
         }
       }
-      console.log('%cStudyCalendar.vue line:190 study', 'color: #007acc;', studyRunningTimeList);
+      return studyRunningTimeList;
+    },
+    getDetailStudyTimeOfSelectedDate(day) {
+      this.showDetailStudyTime = true;
+      this.selectedDate = `${this.year} ${this.month} ${day}`;
+      // 해당 날짜를 이용해 axios 요청하여 그 날의 공부 시간 기록을 가져옴
+      // 가져온 데이터를 parseStudyTimeOfDay를 통해 파싱
+      // 파싱된 결과에 담긴 {startTime, runningTime}을 이용한다
+      const studyRunningTimeList = this.parseStudyTimeOfDay(this.studyTimeCalendar[day].log);
+      // 시작시간에서부터 공부시간을 더한 시간까지 색을 칠한다
+      this.selectedDateStudyHourList = new Array(24);
+      for (let i = 0; i < studyRunningTimeList.length; i++) {
+        const { startTime, runningTime } = studyRunningTimeList[i];
+        const _startTime = new Date(startTime + 'Z');
+        const startHour = _startTime.getHours();
+        const runningHour = runningTime / (1000 * 60 * 60);
+        // array splice 기능 이용해서 해당 시간에 true 삽입
+        for (let j = 0; j < runningHour; j++) {
+          this.selectedDateStudyHourList.splice(startHour + j, 1, true); // 시작시간부터 공부시간만큼 제거
+        }
+      }
+    },
+    getStudyTimeMonth(year, month) {
+      // Todo : 해당 년,월의 공부기록 axios 요청
+      console.log(
+        '%cStudyCalendar.vue line:296 year+',
+        '+month',
+        'color: #007acc;',
+        year + ',' + month
+      );
+      return this.studyTimestamp;
+    },
+    convertStudyTimeMonthToCalendar(array) {
+      // Todo : 캘린더와 인덱스를 맞춰야 함
+      // array 순회하며 공부가 기록된 날 찾기
+      // 기록된날에 작성된 것들을 그 날에 배열 형태로 넣기
+      const calendar = new Array(32); // 1달 최대 31일
+      for (let i = 1; i < calendar.length; i++) {
+        // 캘린더 초기화
+        calendar[i] = [];
+      }
+      for (let i = 0; i < array.length; i++) {
+        // 해당 날짜에 일치하는 정보 삽입
+        const studyInfo = array[i];
+        const day = new Date(studyInfo.startTime).getDate();
+        calendar[day].push(studyInfo);
+      }
+      const result = [];
+      for (let i = 0; i < calendar.length; i++) {
+        const log = calendar[i];
+        if (!log) {
+          result.push(undefined);
+          continue;
+        }
+        result.push({
+          runningTime: this.getRunningTimeByStudyTimestamp(this.parseStudyTimeOfDay(log)),
+          log,
+        });
+      }
+      return result;
+    },
+    getRunningTimeByStudyTimestamp(timestamp) {
+      let sum = 0;
+      for (let i = 0; i < timestamp.length; i++) {
+        sum += timestamp[i].runningTime;
+      }
+      return sum;
+    },
+    convertMsToHour(value) {
+      return parseInt(value / 3600000);
+    },
+    convertRunningTimeToColorLevel(value) {
+      if (value >= 10) return 5;
+      return parseInt(value / 2);
     },
   },
 };
@@ -242,15 +310,61 @@ export default {
   height: 50px;
   width: 50px;
 }
+.day-detail-box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  width: 20px;
+}
 .calendar-pre-month {
   color: gray !important;
-  background-color: lightgray;
+  background-color: lightgray !important;
+  pointer-events: none;
 }
 .calendar-next-month {
   color: gray !important;
-  background-color: lightgray;
+  background-color: lightgray !important;
+  pointer-events: none;
 }
 .calendar-today {
   color: orange;
+}
+.has-study-hour {
+  background-color: orange;
+}
+.tooltip {
+  display: inline-block;
+  font-weight: bold;
+}
+.tooltip-box {
+  display: none;
+  position: absolute;
+  border: 1px solid;
+  border-radius: 5px;
+  color: black;
+  background: white;
+  margin-top: 40px;
+}
+.tooltip:hover {
+  border: 2px solid orange;
+}
+.tooltip:hover .tooltip-box {
+  display: block;
+}
+.orange-1 {
+  background-color: rgb(255, 140, 0, 0.2);
+}
+.orange-2 {
+  background-color: rgb(255, 140, 0, 0.4);
+}
+.orange-3 {
+  background-color: rgb(255, 140, 0, 0.6);
+}
+.orange-4 {
+  background-color: rgb(255, 140, 0, 0.8);
+}
+.orange-5 {
+  background-color: rgb(255, 140, 0, 1);
 }
 </style>
