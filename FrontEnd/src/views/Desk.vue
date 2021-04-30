0478cca46398b450323840230937b58872173e52
이주상 <wntkd0530@naver.com>
2021-04-30 22:34:28 +0900

    feat: 🎸 desk - [FE] 책상 좌우 스크롤 고정 뷰 변경 / 기초 디자인 완료

--- FrontEnd/src/views/Desk.vue
+++ FrontEnd/src/views/Desk.vue
@@ -1,8 +1,12 @@
 <template lang="">
    <div class="desk">
+      <div class="info">
+         <h2 class="userName kyoboHand">안양불빠따</h2>
+         <h3 class="cate kyoboHand">#취업준비생</h3>
+      </div>
       <div class="desk-wrapper">
-         <svg-desk></svg-desk>
          <div class="desk-draw-area"></div>
+         <svg-desk></svg-desk>
       </div>
    </div>
 </template>
@@ -25,7 +29,7 @@ export default {
 <style scoped lang="scss">
 @import 'src/assets/css/common';
 
-$desk-width: 100vmax;
+/* $desk-width: 1280px; */
 
 .desk {
    margin-top: $HeaderHeight;
@@ -33,19 +37,42 @@ $desk-width: 100vmax;
 
    width: 100%;
    height: calc(100% - #{$HeaderHeight});
-   /* background-color: rgb(255, 253, 190); */
 
    display: flex;
    flex-direction: column;
    align-items: center;
    justify-content: center;
 
+   overflow-x: auto;
+   overflow-y: hidden;
+
+   .info {
+      position: fixed;
+      top: $HeaderHeight;
+      left: 50%;
+      transform: translateX(-50%);
+
+      border: 1px dashed red;
+
+      width: 320px;
+      height: 100px;
+
+      text-align: center;
+
+      .cate {
+         margin-top: 5px;
+         color: rgb(150, 150, 150);
+      }
+   }
+
    .desk-wrapper {
       position: relative;
       border: 1px solid red;
 
-      width: $desk-width;
+      width: 1280px;
       height: auto;
+
+      margin: 0 auto;
 
       .desk-draw-area {
          position: absolute;
@@ -54,8 +81,8 @@ $desk-width: 100vmax;
          left: 50%;
          transform: translate(-50%, -50%);
 
-         width: calc(#{$desk-width} * 0.77);
-         height: calc(#{$desk-width} * 0.77 * 5 / 9);
+         width: 1000px;
+         height: 600px;
          border: 1px solid blue;
       }
    }
