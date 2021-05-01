34e8c7a1e05a2db90cef65a6a2cfdaf417a94366
hansol kim <doingnow94@gmail.com>
2021-05-01 23:54:53 +0900

    feat: 🎸 import문 제거
    import문 제거
    
    ✅ Closes: S04P31A401-192

--- BackEnd/RoomAPI/src/main/java/com/mongdok/roomapi/config/RedisConfig.java
+++ BackEnd/RoomAPI/src/main/java/com/mongdok/roomapi/config/RedisConfig.java
@@ -1,10 +1,5 @@
 package com.mongdok.roomapi.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import lombok.var;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +8,6 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * author: pinest94
