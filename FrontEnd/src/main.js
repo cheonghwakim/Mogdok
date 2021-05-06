2d894699e821577c2bcd2e33d663284ea4e1a70c
이주상 <wntkd0530@naver.com>
2021-05-06 19:39:04 +0900

    feat: 🎸 feat/design - [FE] 로티 몽실이 추가 / 메인 구현 완료

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -2,14 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './routes';
 import VueDragscroll from 'vue-dragscroll';
+import VueLottiePlayer from 'vue-lottie-player';
 import store from './store';
 import './utils/filters';
 
 Vue.config.productionTip = false;
 Vue.use(VueDragscroll);
+Vue.use(VueLottiePlayer);
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+   router,
+   store,
+   render: (h) => h(App),
 }).$mount('#app');
--- FrontEnd/src/views/Login.vue
+++ FrontEnd/src/views/Login.vue
@@ -1,58 +1,58 @@
 <template lang="">
-  <div class="login">
-    <div class="container">
-      <div class="content">
-        <svg-mongsil></svg-mongsil>
-        <login-kakao-button></login-kakao-button>
+   <div class="login">
+      <div class="container">
+         <div class="content">
+            <v-lottie-player name="scooterAnim" :width="'250px'" :height="'250px'" loop path="https://assets7.lottiefiles.com/private_files/lf30_z8mfdyic.json" />
+            <login-kakao-button></login-kakao-button>
+         </div>
+         <logo></logo>
       </div>
-      <logo></logo>
-    </div>
-  </div>
+   </div>
 </template>
 <script>
 import LoginKakaoButton from '@/components/LoginKakaoButton';
 import Logo from '@/components/ui/Logo';
-import SvgMongsil from '@/components/svg/SvgMongsil';
 
 export default {
-  name: 'Login',
-  components: { LoginKakaoButton, SvgMongsil, Logo },
-  props: {},
-  data() {
-    return {};
-  },
-  computed: {},
-  watch: {},
-  //lifecycle area
-  methods: {},
+   name: 'Login',
+   components: { LoginKakaoButton, Logo },
+   props: {},
+   data() {
+      return {};
+   },
+   computed: {},
+   watch: {},
+   //lifecycle area
+   methods: {},
 };
 </script>
 <style scoped lang="scss">
 .login {
-  width: 100%;
-  height: 100%;
+   width: 100%;
+   height: 100%;
 
-  display: flex;
-  align-items: center;
-  justify-content: center;
+   display: flex;
+   align-items: center;
+   justify-content: center;
 
-  .container {
-    display: inherit;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    width: 70vw;
-    height: 90vh;
+   .container {
+      display: inherit;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: center;
+      width: 70vw;
+      height: 90vh;
 
-    /* border: 1px solid red; */
+      /* border: 1px solid red; */
 
-    .content {
-      margin-top: 20vh;
+      .content {
+         margin-top: 20vh;
 
-      .kakao {
-        margin-top: 20px;
+         .kakao {
+            margin: 0px auto;
+            margin-top: 20px;
+         }
       }
-    }
-  }
+   }
 }
 </style>
