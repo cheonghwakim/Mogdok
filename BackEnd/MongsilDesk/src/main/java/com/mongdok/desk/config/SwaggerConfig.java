23d884323d08e061a04a5934a262dfb7c46ea1ce
박유정 <1693013@naver.com>
2021-05-16 00:23:38 +0900

    feat: 🎸 feat/deskAPI -[BE] JWT 수정
    JWT 수정

--- BackEnd/MongsilDesk/src/main/java/com/mongdok/desk/config/SwaggerConfig.java
+++ BackEnd/MongsilDesk/src/main/java/com/mongdok/desk/config/SwaggerConfig.java
@@ -1,10 +1,18 @@
 package com.mongdok.desk.config;
 
 import com.google.common.base.Predicates;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,10 +20,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
-                .paths(PathSelectors.any()).build();
-    }
+	@Bean
+	public Docket api() {
+		List<Parameter> global = new ArrayList<>();
+		global.add(new ParameterBuilder()//헤더 auth token 전역에서 사용
+				.name("auth-token")
+				.description("auth-token")
+				.parameterType("header")
+				.required(false)
+				.modelRef(new ModelRef("string"))
+				.build());
+		
+		return new Docket(DocumentationType.SWAGGER_2)
+				.globalOperationParameters(global)
+				.select()
+				.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+				.paths(PathSelectors.any())
+				.build();
+
+	}
 }
