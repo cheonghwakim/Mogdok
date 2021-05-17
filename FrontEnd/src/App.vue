6670efbeb2875cb17d3c4e8c1af07ee118519749
stateaward <stateaward@naver.com>
2021-05-17 13:27:05 +0900

    style: 💄 feat/design - 입구 Door 세로 정렬로 변경 / 사감 크기 조정

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,48 +1,48 @@
 <template>
-  <div id="app">
-    <router-view name="header" />
-    <router-view />
-    <router-view name="footer" />
-  </div>
+   <div id="app">
+      <router-view name="header" />
+      <router-view />
+      <router-view name="footer" />
+   </div>
 </template>
 
 <script>
 export default {
-  name: "App",
-  components: {},
-  props: {},
-  data() {
-    return {};
-  },
-  computed: {},
-  watch: {},
-  //lifecycle area
-  methods: {},
+   name: 'App',
+   components: {},
+   props: {},
+   data() {
+      return {};
+   },
+   computed: {},
+   watch: {},
+   //lifecycle area
+   methods: {},
 };
 </script>
 
 <style lang="scss">
-@import "src/assets/css/common";
-@import "src/assets/css/moveable";
+@import 'src/assets/css/common';
+@import 'src/assets/css/moveable';
 
 #app {
-  width: 100vw;
-  height: 100vh;
+   width: 100vw;
+   height: 100vh;
 
-  display: flex;
-  flex-direction: column;
+   display: flex;
+   flex-direction: column;
 
-  // 스크롤바 스타일링
-  ::-webkit-scrollbar {
-    width: 8px;
-  }
-  ::-webkit-scrollbar-thumb {
-    background-color: rgb(203, 203, 203);
-    border-radius: 10px;
-  }
-  ::-webkit-scrollbar-track {
-    background-color: transparent;
-    border-radius: 20px;
-  }
+   // 스크롤바 스타일링
+   ::-webkit-scrollbar {
+      width: 6px;
+   }
+   ::-webkit-scrollbar-thumb {
+      background-color: rgb(203, 203, 203);
+      border-radius: 10px;
+   }
+   ::-webkit-scrollbar-track {
+      background-color: transparent;
+      border-radius: 20px;
+   }
 }
 </style>
