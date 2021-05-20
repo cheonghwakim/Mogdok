437dc58b6aa555afa01583343c3ca8acd5bf7446
stateaward <stateaward@naver.com>
2021-05-20 16:18:48 +0900

    feat: 🎸 feat/design - 팀소개 버젼 추가

--- FrontEnd/src/components/ui/ModalAbout.vue
+++ FrontEnd/src/components/ui/ModalAbout.vue
@@ -28,6 +28,7 @@
          <div class="powered-wrapper">
             powered by.
             <img src="../../assets/img/about/openvidu.png" alt="" />
+            <p class="version">v1.0.1 2105201820</p>
          </div>
          <btn-rounded class="btnClose" :label="'CLOSE'" :color="'red'" @onClick="closeAbout"></btn-rounded>
       </div>
@@ -233,6 +234,10 @@ export default {
       img {
          margin-top: 20px;
          width: 100px;
+      }
+
+      .version {
+         margin: 20px;
       }
    }
 
