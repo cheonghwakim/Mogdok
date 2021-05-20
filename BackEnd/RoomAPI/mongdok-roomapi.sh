9ca0114a3cdaec1671fd7274f9ed526ec1fdc44e
dudcheol <95_pyc@naver.com>
2021-05-20 23:03:40 +0900

    feat: 🎸 move folder

--- BackEnd/RoomAPI/mongdok-roomapi.sh
+++
@@ -1,3 +0,0 @@
-docker rm -f mongdok_roomapi
-docker run -p 8084:8084 --name mongdok_roomapi --rm --network roomapi-net mongdok_roomapi
-docker ps
--- BackEnd/RoomAPI/src/main/java/com/mongdok/roomapi/repository/StudyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mongdok.roomapi.repository;
-
-import com.mongdok.roomapi.model.StudyLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudyRepository extends JpaRepository<StudyLog, Long> {
-}
